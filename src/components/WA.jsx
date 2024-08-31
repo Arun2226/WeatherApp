9597fefe654b5228828d74cd5d1f800baa80e267
Arun2226 <138017470+Arun2226@users.noreply.github.com>
2024-08-31 15:12:10 +0530

    Update WA.jsx

--- src/components/WA.jsx
+++ src/components/WA.jsx
@@ -38,6 +38,7 @@ function WA() {
 
   return (
     <div className='bg-[#1f213a] dark:bg-white dark:text-black h-screen flex justify-center align-top transition-colors duration-700 ease-in-out'>
+{/*         dark mode button */}
     <button onClick={themeSwitch} className='bg-blue-300 p-2 mt-[20px] rounded-3xl absolute left-[90%] ' >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 dark:hidden">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
